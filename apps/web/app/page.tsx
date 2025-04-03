1f7193c67b2105109e04c4ddc7ca7d8a5aafbbaa
vuongvq <vuong.pw@gmail>
2025-04-03 20:55:14 +0700

    chore: format code

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,22 +1,23 @@
-import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
+import { Button } from '@repo/ui/button'
+import Image, { type ImageProps } from 'next/image'
 
-type Props = Omit<ImageProps, "src"> & {
-  srcLight: string;
-  srcDark: string;
-};
+import styles from './page.module.css'
+
+type Props = Omit<ImageProps, 'src'> & {
+  srcLight: string
+  srcDark: string
+}
 
 const ThemeImage = (props: Props) => {
-  const { srcLight, srcDark, ...rest } = props;
+  const { srcLight, srcDark, ...rest } = props
 
   return (
     <>
       <Image {...rest} src={srcLight} className="imgLight" />
       <Image {...rest} src={srcDark} className="imgDark" />
     </>
-  );
-};
+  )
+}
 
 export default function Home() {
   return (
@@ -98,5 +99,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  );
+  )
 }
